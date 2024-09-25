6573879a3028a04b7ba2c42cfb41b17a9445cfe9
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-09-25 20:37:08 +0300

    store logs of bundled "visionatrix" into files
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- ex_app_scripts/run_visionatrix.sh
+++ ex_app_scripts/run_visionatrix.sh
@@ -3,9 +3,9 @@
 . /etc/environment
 
 if [ "$DISABLE_WORKER" != "1" ]; then
-    /Visionatrix/venv/bin/python -m visionatrix run --mode=SERVER --disable-smart-memory &
+    /Visionatrix/venv/bin/python -m visionatrix run --mode=SERVER > /server.log 2>&1 &
     sleep 15
-    /Visionatrix/venv/bin/python -m visionatrix run --mode=WORKER --disable-smart-memory
+    /Visionatrix/venv/bin/python -m visionatrix run --mode=WORKER --disable-smart-memory > /worker.log 2>&1
 else
-    VIX_SERVER_FULL_MODELS=0 /Visionatrix/venv/bin/python -m visionatrix run --mode=SERVER
+    VIX_SERVER_FULL_MODELS=0 /Visionatrix/venv/bin/python -m visionatrix run --mode=SERVER > /server.log 2>&1
 fi
