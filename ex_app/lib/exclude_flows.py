1a20bbd71f808af2a3cd062e8f1a9a360f63747b
bigcat88 <bigcat88@icloud.com>
2025-05-27 09:45:06 +0300

    sync: all_your_life flow was removed from main repo; photo_stickers2 now will be available in Nextcloud
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

--- ex_app/lib/exclude_flows.py
+++ ex_app/lib/exclude_flows.py
@@ -1,5 +1,3 @@
 EXCLUDE_FLOWS_IDS = [
-    "all_your_life",
-    "photo_stickers2",
     "photomaker_2",
 ]
--- ex_app/lib/exclude_nodes.py
+++ ex_app/lib/exclude_nodes.py
@@ -1,5 +1,3 @@
 EXCLUDE_NODES_IDS = [
-    "https://github.com/Fannovel16/ComfyUI-Frame-Interpolation",
     "https://github.com/shiimizu/ComfyUI-PhotoMaker-Plus",
-    "https://github.com/FizzleDorf/ComfyUI_FizzNodes",
 ]
